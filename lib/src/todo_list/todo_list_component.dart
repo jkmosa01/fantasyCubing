5f58555fcff9341b01775c1cb19d86925b0ab7b1
jkmosa01 <35510468+jkmosa01@users.noreply.github.com>
2019-02-05 15:56:43 -0700

    WCA sign in

--- lib/src/todo_list/todo_list_component.dart
+++ lib/src/todo_list/todo_list_component.dart
@@ -2,8 +2,11 @@ import 'dart:async';
 
 import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
-
+import 'dart:html';
 import 'todo_list_service.dart';
+import 'package:http/http.dart' as http;
+import 'dart:io';
+import 'dart:convert';
 
 @Component(
   selector: 'todo-list',
@@ -30,6 +33,31 @@ class TodoListComponent implements OnInit {
   @override
   Future<Null> ngOnInit() async {
     items = await todoListService.getTodoList();
+    grabWCAMe();
+  }
+
+  void grabWCAMe(){
+    String hash = window.location.hash.substring(1);
+
+    var resultP1 = hash.split('&');
+    String accessToken;
+    for(var i=0; i<resultP1.length; i++){
+      List temp = resultP1[i].split("=");
+      if(temp[0]=='access_token'){
+        accessToken = "Bearer "+temp[1];
+      }
+    }
+    print(accessToken);
+    if(accessToken!=null) {
+      http.get(
+        'https://www.worldcubeassociation.org/api/v0/me',
+        // Send authorization headers to your backend
+        headers: {HttpHeaders.authorizationHeader: accessToken},
+      ).then((resp){
+        var data = json.decode(resp.body);
+        print(data);
+      });
+    }
   }
 
   void add() {
